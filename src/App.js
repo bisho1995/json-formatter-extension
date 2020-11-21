e2ec62693aa60ca2ba2c8e67f57dc0b1534d19f0
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-11-21 17:05:12 +0530

    feat: added redux and react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import React from "react";
 import LinterPage from "@pages/linter/Linter";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-export default LinterPage;
+function rootReducer(state = {}, action) {
+  return state;
+}
+
+const store = createStore(rootReducer);
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Route path='*' component={LinterPage} />
+      </Router>
+    </Provider>
+  );
+}
